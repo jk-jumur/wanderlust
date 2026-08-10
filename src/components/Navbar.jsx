c2c45caec62c7fd0bb52e4d88eb7fce8bc0f765a
jk-jumur <developer.jumur@gmail.com>
2026-08-10 17:20:27 +0600

    update project changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+const Navbar = () => {
+    return (
+      
+       <nav className="flex justify-between m-5 bg-white">
+
+           <ul className="flex  gap-4">
+               <li><Link href='/'>Home</Link></li>
+               <li><Link href='/destinations'>Destinations</Link></li>
+               <li><Link href='/my-booking'>My Bookings</Link></li>
+               <li><Link href='/admin'>Admin</Link></li>
+           </ul>
+                 <div>
+                      <Image 
+                        src={"/assets/Wanderlast.png"}
+                          alt="wanderlust logo"
+                        height={100}
+                        width={100}
+                      />
+                 </div>
+
+
+                  <ul className="flex gap-4">
+                         <li><Link href='/profile'>Profile</Link></li>
+                          <li><Link href='/login'>Login</Link></li>
+                         <li><Link href='/signup'>Sign Up</Link></li>
+             
+           </ul>
+
+       </nav>
+    )
+    
+};
+
+export default Navbar;
