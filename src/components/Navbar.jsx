f666e823c6d3a1586624c1a3d3d0bdb31007947a
jk-jumur <developer.jumur@gmail.com>
2026-08-11 16:58:54 +0600

    Add Destination Form and create Post API

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                <li><Link href='/'>Home</Link></li>
                <li><Link href='/destinations'>Destinations</Link></li>
                <li><Link href='/my-booking'>My Bookings</Link></li>
-               <li><Link href='/admin'>Admin</Link></li>
+               <li><Link href='/add-destination'>Add Destination</Link></li>
            </ul>
                  <div>
                       <Image 
