71798d2eb4b8507611ac25c6d5f906c9cf56a6cb
jk-jumur <developer.jumur@gmail.com>
2026-08-17 03:03:21 +0600

    Store JWT token on the client with BetterAuth

--- src/app/destinations/[id]/page.jsx
+++ src/app/destinations/[id]/page.jsx
@@ -15,14 +15,19 @@ import { LuMapPin } from "react-icons/lu";
 import EditModal from "@/components/EditModal";
 import DeleteDialog from "@/components/DeleteDialog";
 import BookingCard from "@/components/BookingCard";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 
 const DestinationDetailsPage = async ({ params }) => {
   const { id } = await params;
+    const {token} = await auth.api.getToken({
+        headers: await headers()
+    })
 
   // API call to fetch single destination details
   const res = await fetch(`http://localhost:5000/destination/${id}`, {
       headers: {
-          authorization: "logged in"
+        authorization: `Bearer ${token}`
       }
    });
 
